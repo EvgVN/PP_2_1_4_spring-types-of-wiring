8110e5a445aa9fcfa23d2779c21be249d449017d
EvgVN <kolombo@bk.ru>
2023-11-01 05:11:16 +0400

    Egg6-Needle7 На свете есть океан , на океане остров , на острове дерево , на дереве заяц , в зайце утка , в утке яйцо , в яйце иголка , смерть Кощея на игле :(

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,12 +1,20 @@
 package koschei.models;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class Egg6 {
+
+    private Needle7 needle;
+
+    public Egg6(Needle7 needle) {
+        this.needle = needle;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
